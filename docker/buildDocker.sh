eb42b4058ce9c557f9df72ac16671f762a374238
NAJI <najizakaria96@gmail.com>
2020-10-14 11:46:42 +0200

    refactor

---
+++ docker/buildDocker.sh
@@ -0,0 +1,4 @@
+docker build -t zikas1996/formations:latest .
+#docker push zikas1996/formations:latest
+#docker run -d -p 8056:8080 zikas1996/formations:latest
+
--- src/main/java/com/example/security/MyConstants.java
+++ src/main/java/com/example/security/MyConstants.java
@@ -3,12 +3,12 @@ package com.example.security;
 public class MyConstants {
 
     // Replace with your email here:  
-    public static final String MY_EMAIL = "najizakaria1996@gmail.com";
+    public static final String MY_EMAIL = "xxxxxxxxx";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "Zikas2016.";
+    public static final String MY_PASSWORD = "xxxxxxxxx";
 
     // And receiver!
-    public static final String FRIEND_EMAIL = "najizakaria1996@gmail.com";
+    public static final String FRIEND_EMAIL = "xxxxxxxxx";
 
 }
